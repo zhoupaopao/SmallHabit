50895a060aeae1b68613fb4833869836b024c387
zhoupaopao <1312667058@qq.com>
2018-06-06 22:01:45 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a13126/smallhabit/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.a13126.smallhabit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//需要重新安装一下eclipse，重新学习一下教程,根据招聘需求去加
+    //左侧列表了解一下
+
+
+    //微习惯app
+    //制定一个属于自己的计划，每天做5个仰卧起坐，俯卧撑，早起睡觉喝杯水，晚上学习一会代码。以后体重app做完了可以加上体重，需要自定义日历
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
